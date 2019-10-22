2aaa8361086b121152cb7a597429171a9680b327
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-22 21:17:59 +0000

    timer

---
+++ timer1.js
@@ -0,0 +1,15 @@
+const agrv = process.argv.slice(2);
+
+for (let num of agrv) {
+if (num <= 0) {
+  console.log("sorry no one can go back to the past");
+};
+if (!Number(num)) {
+   console.log("no time provided")
+ };
+if (num > 0) {
+  setTimeout(() => {
+      process.stdout.write(`${num} sec `) ;
+        process.stdout.write("\x07")} , num * 1000)
+    };
+};
